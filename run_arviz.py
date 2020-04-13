84604da7e7706d7b1d7cebc43e8b4ff54c05223b
ahartikainen <ahartikainen@github.com>
2020-04-13 15:32:25 +0300

    dict

--- run_arviz.py
+++ run_arviz.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_csv("8school_results.csv")
 
 print(df)
-df_dict = df.to_dict()
+df_dict = df.to_dict(orient="list")
 print(df_dict)
 idata = az.from_dict(df_dict)
 print(idata)
