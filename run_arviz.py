5070a2b910434fec14edb9aa9c1b5aec955e38bb
ahartikainen <ahartikainen@github.com>
2020-04-13 15:35:07 +0300

    reshape

--- run_arviz.py
+++ run_arviz.py
@@ -5,6 +5,7 @@ df = pd.read_csv("8school_results.csv")
 
 print(df)
 df_dict = df.to_dict(orient="list")
+df_dict = {key : np.array(values).reshape(-1, 4).T for key, values in df_dict.items()}
 print(df_dict)
 idata = az.from_dict(df_dict)
 print(idata)
