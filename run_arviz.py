368fa26080c4ae6708359618c2e1e835571ec58d
ahartikainen <ahartikainen@github.com>
2020-04-13 21:19:15 +0300

    fix numpy and sampling

--- run_arviz.py
+++ run_arviz.py
@@ -6,10 +6,19 @@ import pandas as pd
 with open("8school_results.json") as f:
     res = json.load(f)
 
-print(res)
+res = np.array(res)
+print(res.shape)
 res = {'X' : np.swapaxes(res, 0, 1)}
-idata = az.from_dict(df_dict)
+res_warmup = {"X" : res["X"][:, :-100, :]}
+res = {"X" : res["X"][:, -100:, :]}
+print(res.shape)
+print(res_warmup.shape)
+idata = az.from_dict(res)
 print(idata)
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
 print(az.summary(idata))
 
 with open("8school_results_monitor.json") as f:
--- run_rstan.R
+++ run_rstan.R
@@ -9,7 +9,7 @@ schools_dat <- list(J = 8,
                     y = c(28,  8, -3,  7, -1,  1, 18, 12),
 		    sigma = c(15, 10, 16, 11,  9, 11, 10, 18))
 
-fit <- stan(file = 'Stan-models/8schools.stan', data = schools_dat)
+fit <- stan(file = 'Stan-models/8schools.stan', data = schools_dat, chains = 4, iter = 300, warmup = 200)
 
 print(fit)
 
