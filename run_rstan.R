a38a85b18016f98a6fd6478f6201bd50a8b7a956
Ari Hartikainen <ari.hartikainen@aalto.fi>
2020-04-13 08:00:31 +0300

    add 8 schools

---
+++ run_rstan.R
@@ -0,0 +1,13 @@
+library("rstan")
+
+options(mc.cores = parallel::detectCores())
+
+rstan_options(auto_write = TRUE)
+
+schools_dat <- list(J = 8,
+                    y = c(28,  8, -3,  7, -1,  1, 18, 12),
+		    sigma = c(15, 10, 16, 11,  9, 11, 10, 18))
+
+fit <- stan(file = 'Stan-models/8schools.stan', data = schools_dat)
+
+print(fit)
