606ba62c473343db73fbaef95bc743b09bd8eb56
Ari Hartikainen <ari.hartikainen@ramboll.fi>
2020-04-14 13:51:44 +0300

    reset index

--- run_arviz.py
+++ run_arviz.py
@@ -8,16 +8,16 @@ with open("8school_results.json") as f:
 
 res = np.array(res)
 print(res.shape)
-res = {'X' : np.swapaxes(res, 0, 1)}
-res_warmup = {"X" : res["X"][:, :-100, :]}
-res = {"X" : res["X"][:, -100:, :]}
+res = {"X": np.swapaxes(res, 0, 1)}
+res_warmup = {"X": res["X"][:, :-100, :]}
+res = {"X": res["X"][:, -100:, :]}
 print(res["X"].shape)
 print(res_warmup["X"].shape)
 idata = az.from_dict(res)
 print(idata)
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", None)
 
 print(az.summary(idata))
 
@@ -28,7 +28,7 @@ res_monitor = pd.DataFrame.from_records(res_monitor, index="_row")
 res_monitor.index.name = None
 print(res_monitor)
 
-reference = pd.read_csv("./reference_values.csv", index_col=0).sort_index(axis=1).sort_index(axis=0)
+reference = pd.read_csv("./reference_values.csv", index_col=0).reset_index()
 
 # test arviz functions
 funcs = {
@@ -49,11 +49,13 @@ funcs = {
     "mcse_quantile30": lambda x: az.mcse(x, method="quantile", prob=0.3),
 }
 results = {}
-for key, coord_dict, vals in az.plots.plot_utils.xarray_var_iter(idata.posterior, combined=True):
+for key, coord_dict, vals in az.plots.plot_utils.xarray_var_iter(
+    idata.posterior, combined=True
+):
     if coord_dict:
         key = "{}".format(list(coord_dict.values())[0] + 1)
     results[key] = {func_name: func(vals) for func_name, func in funcs.items()}
-arviz_data = pd.DataFrame.from_dict(results).T
+arviz_data = pd.DataFrame.from_dict(results).T.reset_index()
 
 # check column names
 print("Column names are the same:", set(arviz_data.columns) == set(reference.columns))
@@ -63,15 +65,17 @@ print("Column names are the same:", set(arviz_data.columns) == set(reference.col
 # this is due to numerical accuracy in calculation leading to rankdata
 # function, which scales minimal difference to larger scale
 # test first with numpy
+print("REFERENCE")
 print(reference)
+print("ARVIZ")
 print(arviz_data)
-print(reference-arviz_data)
-print((reference-arviz_data).max(0))
-print((reference-arviz_data).max(1))
-print((reference-arviz_data).max().max())
+print(reference - arviz_data)
+print((reference - arviz_data).max(0))
+print((reference - arviz_data).max(1))
+print((reference - arviz_data).max().max())
 # then test manually (more strict)
-#assert (abs(reference["rhat_rank"] - arviz_data["rhat_rank"]) < 6e-5).all(None)
-#assert abs(np.median(reference["rhat_rank"] - arviz_data["rhat_rank"]) < 1e-14).all(None)
-#not_rhat = [col for col in reference.columns if col != "rhat_rank"]
-#assert (abs((reference[not_rhat] - arviz_data[not_rhat])).values < 1e-8).all(None)
-#assert abs(np.median(reference[not_rhat] - arviz_data[not_rhat]) < 1e-14).all(None)
+# assert (abs(reference["rhat_rank"] - arviz_data["rhat_rank"]) < 6e-5).all(None)
+# assert abs(np.median(reference["rhat_rank"] - arviz_data["rhat_rank"]) < 1e-14).all(None)
+# not_rhat = [col for col in reference.columns if col != "rhat_rank"]
+# assert (abs((reference[not_rhat] - arviz_data[not_rhat])).values < 1e-8).all(None)
+# assert abs(np.median(reference[not_rhat] - arviz_data[not_rhat]) < 1e-14).all(None)
