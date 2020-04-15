219b0caf723a8469a26a51812e4efacc950e2d4d
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2020-04-15 08:32:45 +0300

    Create run_pystan.py

---
+++ run_pystan.py
@@ -0,0 +1,18 @@
+import arviz as az
+import pystan
+
+schools_dat = dict(
+    J = 8,
+    y = [28,  8, -3,  7, -1,  1, 18, 12],
+    sigma = [15, 10, 16, 11,  9, 11, 10, 18]
+)
+
+model = pystan.StanModel(file="Stan-models/8schools.stan", extra_compile_args=['-O3'])
+
+fit = model.sampling(data=schools_dat, chains=4, iter=300, warmup=200, seed=123)
+
+print(fit)
+
+print(az.summary(fit))
+
+print(fit.extract(permuted=False)
