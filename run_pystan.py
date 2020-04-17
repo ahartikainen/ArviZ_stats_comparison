cc3c6c2c7a86c3e4c1036eb92356fafc9a5f9468
Ari Hartikainen <ari.hartikainen@ramboll.fi>
2020-04-17 15:47:33 +0300

    add native

--- run_pystan.py
+++ run_pystan.py
@@ -6,7 +6,7 @@ schools_dat = dict(
     sigma = [15, 10, 16, 11,  9, 11, 10, 18]
 )
 
-model = pystan.StanModel(file="Stan-models/8schools.stan", extra_compile_args=['-O3'])
+model = pystan.StanModel(file="Stan-models/8schools.stan", extra_compile_args=['-O3', "-march=native", "-mtune=native"])
 
 fit = model.sampling(data=schools_dat, chains=2, iter=300, warmup=200, seed=123, chain_id=[1,2], n_jobs=1, verbose=True)
 
