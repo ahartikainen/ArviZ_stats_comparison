bf826684c30f489e5aa89e671e07ca2cfeea01de
ahartikainen <ahartikainen@github.com>
2020-08-30 22:33:39 +0300

    rename

--- create_viz.py
+++ create_viz.py
@@ -9,7 +9,7 @@ from bokeh.models import HoverTool
 if os.environ.get("USEGIT") == "true":
     env_name = "git"
 else:
-    env_name = "pip-cran"
+    env_name = "pypi-cran"
 
 df_r = pd.read_csv("reference_posterior.csv", index_col=0).reset_index(drop=True)
 df_py = (
@@ -137,4 +137,4 @@ boxwhisker3.opts(
 
 layout = hv.Layout((boxwhisker, boxwhisker2, boxwhisker3)).cols(1)
 renderer = hv.renderer("bokeh")
-renderer.save(layout, f"saved_boxplots_{env_name}")
+renderer.save(layout, f"posterior_arviz_convergence_{env_name}")
